330e1d5c0ad5eb2a1660bd5c0257ce81bebce8b2
daffathirabbaniin <andikadaffafathirabbani04@gmail.com>
2025-11-02 22:19:33 +0700

    nambah gambar

--- detail.js
+++ detail.js
@@ -25,9 +25,7 @@ const dataProduk = {
             "assets/Chili3.jpg"
         ]
     },
-
-
-    jagung: {
+      jagung: {
         nama: "Benih jagung",
         harga: "Rp 500",
         hargaLama: "Rp 750",
@@ -40,6 +38,100 @@ const dataProduk = {
             "assets/Chili3.jpg"
         ]
     },
+
+
+    sawi: {
+    nama: "Benih Sawi",
+    harga: "Rp 1.500",
+    hargaLama: "Rp 2.000",
+    deskripsi: "Bibit Sawi unggul dengan daya tumbuh di atas 90%, tahan terhadap hama dan penyakit, serta menghasilkan daun yang hijau segar, tebal, dan bernutrisi tinggi.",
+    gambar: [
+        "assets/sawi.jpeg",
+        "assets/sawi2.jpeg",
+        "assets/sawi3.jpeg"
+    ]
+},
+
+pare: {
+    nama: "Benih pare",
+    harga: "Rp 1.500",
+    hargaLama: "Rp 2.000",
+    deskripsi: "Bibit pare unggul dengan daya tumbuh di atas 90%, tahan terhadap hama dan penyakit, serta menghasilkan daun yang hijau segar, tebal, dan bernutrisi tinggi.",
+    gambar: [
+        "assets/cover pare.jpeg",
+        "assets/sawi2.jpeg",
+        "assets/sawi3.jpeg"
+    ]
+},
+
+
+
+    Kangkung: {
+        nama: "Benih kangkung",
+        harga: "Rp 500",
+        hargaLama: "Rp 750",
+        deskripsi: "Bibit Kangkung unggul dengan daya tumbuh tinggi di atas 90%, tahan terhadap hama dan cuaca ekstrem, serta menghasilkan daun yang hijau segar dan lebat.",
+
+        
+        gambar: [
+            "assets/cover.jpeg",
+            "assets/Chili2.jpg",
+            "assets/Chili3.jpg"
+        ]
+    },
+
+    tomat: {
+        nama: "Benih Tomat",
+        harga: "Rp 1.000",
+        hargaLama: "Rp 1.500",
+        deskripsi: "Benih tomat unggul yang cepat tumbuh, tahan penyakit, dan menghasilkan buah merah segar dengan rasa manis alami.",
+        gambar: [
+            "assets/tomat cover.jpeg",
+            "assets/tomat2.jpeg",
+            "assets/tomat3.jpeg"
+        ]
+    },
+
+    terong: {
+        nama: "Benih Terong",
+        harga: "Rp 1.000",
+        hargaLama: "Rp 1.500",
+        deskripsi: "Benih terong unggul dengan daya tumbuh tinggi, tahan cuaca ekstrem, dan menghasilkan buah besar serta mengkilap.",
+        gambar: [
+            "assets/cover terong.jpeg",
+            "assets/terong2.jpeg",
+            "assets/terong3.jpeg"
+        ]
+    },
+
+    mentimun: {
+        nama: "Benih Mentimun",
+        harga: "Rp 800",
+        hargaLama: "Rp 1.200",
+        deskripsi: "Benih mentimun unggul dengan hasil panen tinggi, tahan penyakit, dan menghasilkan buah renyah serta segar.",
+        gambar: [
+            "assets/ cover mentimun.jpeg",
+            "assets/mentimun2.jpeg",
+            "assets/mentimun3.jpeg"
+        ]
+    },
+
+    kacangpanjang: {
+        nama: "Benih Kacang Panjang",
+        harga: "Rp 1.000",
+        hargaLama: "Rp 1.500",
+        deskripsi: "Benih kacang panjang unggul dengan daya tumbuh tinggi, tahan penyakit, dan menghasilkan polong panjang serta renyah.",
+        gambar: [
+            "assets/ cover_kacang.jpeg",
+            "assets/kacangpanjang2.jpeg",
+            "assets/kacangpanjang3.jpeg"
+        ]
+    },
+
+
+
+
+
 };
 
 const url = new URLSearchParams(window.location.search);
